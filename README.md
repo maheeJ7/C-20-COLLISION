a16aadc37d918dd08900134c92d4acd9da708432
maheeJ7 <73384539+maheeJ7@users.noreply.github.com>
2020-11-03 14:55:58 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C-20-COLLISION
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(400,400);
+  fixedRect = createSprite(200, 200, 50, 50);
+  movingRect = createSprite(300,200,50,50);
+  fixedRect.shapeColor = "red";
+  movingRect.shapeColor="red";
+  
+}
+
+function draw() {
+
+  background("yellow"); 
+
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+
+  if(movingRect.x-fixedRect.x<=fixedRect.width/2+movingRect.width/2 &&
+    fixedRect.x-movingRect.x<=fixedRect.width/2+movingRect.width/2 && 
+    movingRect.y-fixedRect.y<=fixedRect.height/2+movingRect.height/2 &&
+    fixedRect.y-movingRect.y<=fixedRect.height/2+movingRect.height/2){
+    fixedRect.shapeColor = "green";
+  movingRect.shapeColor="green"; 
+  }
+else{
+fixedRect.shapeColor = "red";
+movingRect.shapeColor="red";
+}
+
+  
+  drawSprites();
+}
